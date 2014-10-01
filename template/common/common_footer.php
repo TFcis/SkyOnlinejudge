840543b8714eed95fcca31b4e4c0ff37677e863b
LFsWang <tnst92002@gmail.com>
2014-10-01 12:34:13 +0000

    add nav css

---
+++ template/common/common_footer.php
@@ -0,0 +1,7 @@
+<?php
+	if(!defined('IN_TEMPLATE'))
+    {
+      exit('Access denied');
+    }
+?>
+</body>
---
+++ template/common/common_header.php
@@ -0,0 +1,21 @@
+<?php
+	if(!defined('IN_TEMPLATE'))
+    {
+      exit('Access denied');
+    }
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset='utf-8'>
+    <title>TNFSH Online Judge</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+  	<script type="text/x-mathjax-config">
+  		MathJax.Hub.Config(
+        {	tex2jax:
+           	{
+               	inlineMath: [['$','$'], ['\\(','\\)']],
+                processEscapes: true
+            }
+        });
+	</script>
+    <script type="text/javascript" src="js/MathJax/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
