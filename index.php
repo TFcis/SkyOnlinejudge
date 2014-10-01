a38be3855ddaf8fd68269946c71f52a1ae80cb69
LFsWang <tnst92002@gmail.com>
2014-10-01 12:36:43 +0000

    add css

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+define('IN_TEMPLATE',1);
+require_once('template/common/common_header.php');
+//it may be need a template
+echo '</head>';
+echo '<body>';
+?>
+
+
+<nav class="nav" role="navigation">
+  <div class="container">
+  	<a href="">TNFSH Online Judge(Test)</a>
+    <ul>
+	  <li><a href="http://forum.tfcis.org/forum.php?mod=group&fid=107" target="_new">Discuss</a></li>
+    </ul>
+  </div>
+</nav>
+
+<h1>HIHI</h1>
+Math Test<br>
+$a,\ b(a,\ b\ <\ 2^{63})$
+  
+<?php
+require_once('template/common/common_footer.php');
+?>
