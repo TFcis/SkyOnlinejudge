28c61bf874c5ed27c64ace34e13f83a6ade21b88
LFsWang <tnst92002@gmail.com>
2014-10-22 06:54:58 +0000

    modify index

--- template/index/index.php
+++ template/index/index.php
@@ -11,9 +11,7 @@ if(!defined('IN_TEMPLATE'))
 Math Test<br>
 $a,\ b(a,\ b\ <\ 2^{63})$
 <br>
-<?php
-    var_dump($_G);
-?>
+
 <h1>MUSIC LOGIN ONLY~</h1>
 <?php if($_G['uid']): ?>
 
@@ -22,6 +20,6 @@ $a,\ b(a,\ b\ <\ 2^{63})$
   <source src="http://pc2.tfcis.org:81/lfs/20140830/egg.ogg">
   HTML5 ONLY! 請升級您的瀏覽器
 </audio>
-
-
+<br>
+<iframe width="560" height="315" src="//www.youtube.com/embed/videoseries?list=PLvLX2y1VZ-tEmqtENBW39gdozqFCN_WZc" frameborder="0" allowfullscreen></iframe>
 <?php endif; ?>
