5c8ef5a2fe664850024396f814ef438e4571d393
LFsWang <tnst92002@gmail.com>
2014-10-22 05:56:53 +0000

    YAA

---
+++ function/Skyoj.lib.php
@@ -0,0 +1,42 @@
+<?php
+if(!defined('IN_SKYOJSYSTEM'))
+{
+  exit('Access denied');
+}
+
+//BASIC
+function throwjson($status,$data)
+{
+    exit(json_encode(array('status'=>$status,'data'=>$data)));
+}
+
+function expand_userlist($string)
+{
+    $tmp = explode(',',$string);
+    $users = array();
+    foreach($tmp as $user)
+    {
+        if( intval($user) == $user)
+        {
+            $users[]=intval($user);
+        }
+        if( preg_match('/(\d+)-(\d+)/',$user,$match) )
+        {
+            $a = intval($match[1]);
+            $b = intval($match[2]);
+            if($a && $b)
+            {
+                if($a > $b)
+                    list($a,$b) = array($b,$a);
+                for(;$a<=$b;$a++)
+                {
+                    $users[]=$a;
+                }
+            }
+        }
+    }
+    
+    sort($users);
+    //var_dump($users);
+    return array_unique($users);
+}
