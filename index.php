32df3f7e33125f150e8bf8369b93d35abe273ca3
LFsWang <tnst92002@gmail.com>
2015-02-07 03:10:56 +0000

    Add ace editor

--- index.php
+++ index.php
@@ -4,5 +4,7 @@ require_once('function/user/user.lib.php');
 
 if(isset($_GET['old']))
 Render::render('index_1','index');
+elseif(isset($_GET['test']))
+Render::render('common_codepanel');
 else
 Render::render('index','index');
