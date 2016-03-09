276d0376b16074e43fcbff8f7b2e02ece6c8815d
Xi-Plus <huangxuanyuxiplus@gmail.com>
2016-03-09 14:07:35 +0800

    Update config.example.php

--- config/config.example.php
+++ config/config.example.php
@@ -1,11 +1,11 @@
 <?php
 $_config = array();
 // ----------------------------  CONFIG DB  ----------------------------- //
-$_config['db']['dbhost'] = 'localhost';
-$_config['db']['dbuser'] = 'root';
-$_config['db']['dbpw'] = '';
-$_config['db']['dbcharset'] = 'utf8';
-$_config['db']['dbname'] = 'test';
-$_config['db']['tablepre'] = 'tojtest';
-$_config['cookie']['namepre'] = 'tojtest';
+$_config['db']['query_string']  = 'mysql:host=localhost;dbname=test';
+$_config['db']['dbuser']        = 'root';
+$_config['db']['dbpassword']    = '';
+$_config['db']['dbcharset']     = 'utf8';
+$_config['db']['dbname']        = 'test';
+$_config['db']['tablepre']      = 'tojtest';
+$_config['cookie']['namepre']   = 'tojtest';
 ?>
