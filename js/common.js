51fbf9ca530260a4e2a29f3fd955e5bb7e372329
LFsWang <tnst92002@gmail.com>
2016-08-04 17:46:25 +0800

    Fix! API submit block javascript

--- js/common.js
+++ js/common.js
@@ -44,7 +44,7 @@ function api_submit(url,fmid,showid,succ,err)
             if (typeof succ != 'undefined')
                 succ(res);
         }
-    },"json").on("error",function(e){
+    },"json").fail(function(e){
         console.log(e);
     });
 }
