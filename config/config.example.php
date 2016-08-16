3b7c99873d7ebd2e056ce30f74c7618daadccd70
LFsWang <11097@gm.tnfsh.tn.edu.tw>
2016-08-16 20:29:58 +0800

    Fix tablepre style

--- config/config.example.php
+++ config/config.example.php
@@ -5,6 +5,6 @@ $_config['db']['query_string']  = 'mysql:host=localhost;dbname=sky';
 $_config['db']['dbuser']        = 'root';
 $_config['db']['dbpassword']    = '';
 $_config['db']['dbcharset']     = 'utf8';
-$_config['db']['tablepre']      = 'tojtest';
+$_config['db']['tablepre']      = 'tojtest_';
 $_config['cookie']['namepre']   = 'tojtest';
 ?>
