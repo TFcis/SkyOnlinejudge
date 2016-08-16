4f51a1c1064705e86d92e9c813fac444e52f9827
LFsWang <tnst92002@gmail.com>
2016-08-16 21:00:23 +0800

    Fix

--- config/config.example.php
+++ config/config.example.php
@@ -2,10 +2,9 @@
 
 $_config = [];
 // ----------------------------  CONFIG DB  ----------------------------- //
-$_config['db']['dbhost'] = 'localhost';
-$_config['db']['dbuser'] = 'root';
-$_config['db']['dbpw'] = '';
-$_config['db']['dbcharset'] = 'utf8';
-$_config['db']['dbname'] = 'test';
-$_config['db']['tablepre'] = 'tojtest';
-$_config['cookie']['namepre'] = 'tojtest';
+$_config['db']['query_string']  = 'mysql:host=localhost;dbname=sky';
+$_config['db']['dbuser']        = 'root';
+$_config['db']['dbpassword']    = '';
+$_config['db']['dbcharset']     = 'utf8';
+$_config['db']['tablepre']      = 'tojtest_';
+$_config['cookie']['namepre']   = 'tojtest';
