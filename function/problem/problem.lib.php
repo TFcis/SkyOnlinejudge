708f988643c2fb7b245ebf5e95b2e4adf9477499
LFsWang <tnst92002@gmail.com>
2016-09-15 02:04:49 +0800

    新增AC Rate(TIOJ Style ICON)

--- function/problem/problem.lib.php
+++ function/problem/problem.lib.php
@@ -13,3 +13,48 @@ function CreateNewProblemID(int $owner,string $title):int
                          VALUES (NULL,?,?)",$owner,$title);
     return $res?\DB::lastInsertId('pid'):null;
 }
+
+function ProblemSubmitNum(int $pid ,int $uid = 0,bool $update = false):int
+{
+    static $val = [];
+    static $table;
+    if( !isset($val[$uid]) ) $val[$uid] = [];
+    if( isset($val[$uid][$pid]) && !$update )return $val[$uid][$pid];
+    if( !isset($table) ) $table = \DB::tname('challenge');
+
+    if( $uid!==0 )
+        $d = \DB::fetchEx("SELECT COUNT(*) FROM `{$table}` WHERE `uid`=? AND `pid`=? ",$uid,$pid);
+    else
+        $d = \DB::fetchEx("SELECT COUNT(*) FROM `{$table}` WHERE `pid`=? ",$pid);
+
+    return $d===false ? 0 : ($val[$uid][$pid] = $d[0]);
+}
+
+function ProblemStateNum(int $pid ,int $state,int $uid = 0,bool $update = false):int
+{
+    static $val = [];
+    static $table;
+    if( !isset($val[$uid]) ) $val[$uid] = [];
+    if( isset($val[$uid][$pid]) && !$update )return $val[$uid][$pid];
+    if( !isset($table) ) $table = \DB::tname('challenge');
+
+    if( $uid!==0 )
+        $d = \DB::fetchEx("SELECT COUNT(*) FROM `{$table}` WHERE `uid`=? AND `pid`=? AND `result`=?",$uid,$pid,$state);
+    else
+        $d = \DB::fetchEx("SELECT COUNT(*) FROM `{$table}` WHERE `pid`=? AND `result`=?",$pid,$state);
+
+    return $d===false ? 0 : ($val[$uid][$pid] = $d[0]);
+}
+
+function UserProblemState(int $pid ,int $uid,bool $update = false):int
+{
+    static $val = [];
+    static $table;
+    if( !isset($val[$uid]) ) $val[$uid] = [];
+    if( isset($val[$uid][$pid]) && !$update )return $val[$uid][$pid];
+    if( !isset($table) ) $table = \DB::tname('challenge');
+
+    $d = \DB::fetchEx("SELECT MIN(`result`) FROM `{$table}` WHERE `uid`=? AND `pid`=?",$uid,$pid);
+
+    return $d===false || !isset($d[0]) ? 0 : ($val[$uid][$pid] = $d[0]);
+}
