944d6f1f689e8ac78f4fd0a315844a5f214712da
LFsWang <tnst92002@gmail.com>
2016-12-20 15:46:43 +0800

    clear page

--- template/contest/view_problem.php
+++ template/contest/view_problem.php
@@ -6,19 +6,9 @@ if (!defined('IN_TEMPLATE')) {
 
 <div class="container">
     <div class="row">
-        <div class="col-md-9">
-            <h3><?=$tmpl['problem']->pid()?>. <?=\SKYOJ\html($tmpl['problem']->GetTitle())?></h3>
-        </div>
-        <div class="col-md-3 text-right">
-            <p><?=array_search($tmpl['problem']->GetJudgeType(),SKYOJ\ProblemJudgeTypeEnum::getConstants())?> Judge</p>
-            <p>Open(Stats)</p>
-        </div>
-    </div>
-    <hr>
-    <div class="row">
         <div class="col-md-12">
             <?=$tmpl['problem']->GetRenderedContent()?>
-        </div><!--Main end-->
+        </div>
     </div>
     <br>
 </div>
