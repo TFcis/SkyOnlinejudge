f7665319496a24cc27d5247cc04e0d4128a5d4bd
lys0829 <lys900829@gmail.com>
2017-05-20 00:33:16 +0800

    add filter

--- function/admin/admin_userlist.php
+++ function/admin/admin_userlist.php
@@ -10,13 +10,15 @@ function userlistHandle(){
     global $_E;
 
     $page = (int)\SKYOJ\safe_get('page');
+    $search_nick = (string)\SKYOJ\safe_get('nickname');
     $_E['template']['userlist'] = [];
-    $pl = new PageList('account');
+    $pl = new PageList('account','`nickname` LIKE '.'\'%'.$search_nick.'%\'');
     $data = $pl->GetPageDataByPage($page,'uid','*');
     if ($data !== false) {
         $_E['template']['userlist'] = $data;
         $_E['template']['userlist_page_list'] = $pl;
         $_E['template']['userlist_now'] = $page;
+        $_E['template']['userlist_search'] = $search_nick;
     }
     \Render::renderSingleTemplate('admin_userlist', 'admin');
 }
--- template/admin/admin_userlist.php
+++ template/admin/admin_userlist.php
@@ -3,35 +3,58 @@ if (!defined('IN_TEMPLATE')) {
     exit('Access denied');
 }
 ?>
+
+<script>
+$(document).ready(function()
+{
+    $("#search").submit(function(e)
+    {
+        e.preventDefault();
+        search_nick = $("#nickname").val();
+        loadTemplate('userlist?page=1&nickname='+search_nick);
+    });
+});
+</script>
+
 <div class="container">
 
-    <div>
-        <div class="page-header">
-            <h1>Userlist<small>Manage User</small></h1>
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="page-header">
+                <h1>Userlist<small>Manage User</small></h1>
+                <form class="form-inline" action="admin.php" id="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="nickname" placeholder="nickname">
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </div>
+                </form>
+            </div>
+            <table class = "table">
+                <thead>
+                    <tr>
+                        <th style = "width: 90px" >Uid</th>
+                        <th style = "width: 180px">Nickname</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($tmpl['userlist'] as $row) { ?>
+                    <tr>
+                        <td><?=htmlentities($row['uid'])?></td>
+                        <td><a href="<?=$SkyOJ->uri('user','view',$row['uid'])?>"><?=htmlentities($row['nickname'])?></a></td>
+                    </tr>
+                <?php } ?>
+                </tbody>
+            
+            </table>
+            <center>
+            <?php Render::renderPagination(
+            $_E['template']['userlist_page_list'],
+            "userlist?page=%d&nickname=".$_E['template']['userlist_search'],
+            $_E['template']['userlist_now'],
+            true) ?>
+            </center>
         </div>
-        <table class = "table">
-        <thead>
-            <tr>
-                <th style = "width: 90px" >Uid</th>
-                <th style = "width: 180px">Nickname</th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($tmpl['userlist'] as $row) { ?>
-            <tr>
-                <td><?=htmlentities($row['uid'])?></td>
-                <td><a href="<?=$SkyOJ->uri('user','view',$row['uid'])?>"><?=htmlentities($row['nickname'])?></a></td>
-            </tr>
-        <?php } ?>
-        </tbody>
-        
-        </table>
-        <center>
-        <?php Render::renderPagination(
-        $_E['template']['userlist_page_list'],
-        "userlist?page=%d&pagelist",
-        $_E['template']['userlist_now'],
-        true) ?>
-        </center>
     </div>
 </div>
