01ebbaaa91d0bdc8c31b6d7b09e5f0866ddd83e6
LFsWang <7088579+LFsWang@users.noreply.github.com>
2018-06-09 15:29:54 +0800

    add old index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php namespace SKYOJ;
+require_once 'GlobalSetting.php';
+require_once 'function/SkyOJ.php';
+$SkyOJ->RegisterHandle('index','\\SKYOJ\\Index',null,true);
+$SkyOJ->RegisterHandle('code' ,'\\SKYOJ\\Code\\CodeHandle',$_E['ROOT'].'/code.php');
+$SkyOJ->RegisterHandle('rank' ,'\\SKYOJ\\Rank\\RankHandle',$_E['ROOT'].'/function/rank/rank.php');
+$SkyOJ->RegisterHandle('admin','\\SKYOJ\\Admin\\AdminHandle',$_E['ROOT'].'/admin.php');
+$SkyOJ->RegisterHandle('user','\\SKYOJ\\User\\UserHandle',$_E['ROOT'].'/user.php');
+$SkyOJ->RegisterHandle('problem','\\SKYOJ\\Problem\\ProblemHandle',$_E['ROOT'].'/function/problem/problem.php');
+$SkyOJ->RegisterHandle('chal','\\SKYOJ\\Challenge\\ChallengeHandle',$_E['ROOT'].'/challenge.php');
+$SkyOJ->RegisterHandle('contest','\\SKYOJ\\Contest\\ContestHandle',$_E['ROOT'].'/function/contest/contest.php');
+$SkyOJ->run();
+function Index(){
+    global $SkyOJ;
+    $param = $SkyOJ->UriParam(1);
+    switch($param){
+        case 'old':
+            \Render::render('index_1', 'index');
+            break;
+        default:
+            \Render::render('index', 'index');
+            break;
+    }
+}
