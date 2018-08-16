d67b2faea4053ae160bd6a556b81297564a5db34
LFsWang <7088579+LFsWang@users.noreply.github.com>
2018-08-16 20:47:22 +0800

    fix judge info in contest

--- template/contest/view_problem.php
+++ template/contest/view_problem.php
@@ -8,6 +8,13 @@ if (!defined('IN_TEMPLATE')) {
     <div class="row">
         <div class="col-md-12">
             <?=$tmpl['problem']->getRendedContent()?>
+            <br/>
+            <h2>Judge Setting</h2>
+            run-time limit: <?=$tmpl['problem']->runtime_limit?> ms
+            <br/>
+            memory limit: <?=$tmpl['problem']->memory_limit?> byte
+            <br/>
+            測資數量: <?=count($tmpl['problem']->getTestdataInfo())?> 
         </div>
     </div>
     <br>
