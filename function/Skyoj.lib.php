0d1375e78014483400859fc5c22af8253780b48f
lys0829 <lys900829@gmail.com>
2018-09-01 20:46:43 +0800

    修改IP取得標準

--- function/Skyoj.lib.php
+++ function/Skyoj.lib.php
@@ -513,12 +513,6 @@ function human_filesize($bytes, $decimals = 2)
 //not safe method
 function get_ip()
 {
-    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
-        $ip = $_SERVER['HTTP_CLIENT_IP'];
-    }else if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    }else{
-        $ip = $_SERVER['REMOTE_ADDR'];
-    }
+    $ip = $_SERVER['REMOTE_ADDR'];
     return $ip;
 }
