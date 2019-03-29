dfe09f47f95180bb506733d7873b8cbfd4b81446
lys0829 <lys900829@gmail.com>
2019-03-29 23:23:57 +0800

    prevent race condition

--- function/rank/rank_api_rebuild.php
+++ function/rank/rank_api_rebuild.php
@@ -8,6 +8,9 @@ function rank_api_rebuildHandle()
     global $SkyOJ,$_E,$_G;
 
     try{
+        set_time_limit(0);
+        ignore_user_abort(true);
+        
         $sb_id = $SkyOJ->UriParam(3);
 
         $user = $SkyOJ->UriParam(4);
@@ -26,8 +29,11 @@ function rank_api_rebuildHandle()
         {
             if($sb->rebuildAllable($SkyOJ->User))
             {
-                $sb->rebuild();
-                \SKYOJ\throwjson('SUCC','yes');
+                $res = $sb->rebuild();
+                if($res[0])
+                    \SKYOJ\throwjson('SUCC','yes');
+                else
+                    throw new \Exception($res[1]);
             }
             else
             {
@@ -36,8 +42,11 @@ function rank_api_rebuildHandle()
         }
         else if( $sb->rebuildUserable($SkyOJ->User,$user) )
         {
-            $sb->rebuild([$user]);
-            \SKYOJ\throwjson('SUCC','yes');
+            $res = $sb->rebuild();
+            if($res[0])
+                \SKYOJ\throwjson('SUCC','yes');
+            else
+                throw new \Exception($res[1]);
         }
         throw new \Exception('Access denied');
     }catch(\Exception $e){
