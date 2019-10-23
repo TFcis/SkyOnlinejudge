8a1c484cf416c9b45f568fca740009b5114bdb0d
Saya <sa0726866@gmail.com>
2019-10-23 16:46:54 +0800

    Update ScoreBoardTypeEnum.php

--- SkyOJ/Scoreboard/ScoreBoardTypeEnum.php
+++ SkyOJ/Scoreboard/ScoreBoardTypeEnum.php
@@ -4,3 +4,10 @@ class ScoreBoardTypeEnum extends \SkyOJ\Helper\Enum
 {
     const ScoreBoard = 1;
 }
+
+class ScoreBoardAllowJoinEnum extends \SkyOJ\Helper\Enum
+{
+    const NotAllowed = 0;
+    const Allowed = 1;
+    //const NeedJudge = 2;
+}
